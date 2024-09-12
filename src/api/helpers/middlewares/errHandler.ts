f1f133bc54db5ff469851f0a40828864fa3749ab
beauspot <beauspot@outlook.com>
2024-09-12 23:56:17 +0100

    slightly modified this files

--- src/api/helpers/middlewares/errHandler.ts
+++ src/api/helpers/middlewares/errHandler.ts
@@ -1,15 +1,17 @@
-import { Request, Response } from "express";
+import { Request, Response, NextFunction } from "express";
 import { CustomAPIError } from "@/helpers/utils/custom-errors";
 const errorHandlerMiddleware = (
   err: Error,
   req: Request,
   res: Response,
+  next: NextFunction
 ) => {
   if (err instanceof CustomAPIError) {
     return res.status(err.statusCode).json({ msg: err.message });
   }
-  console.log(err);
-  return res.status(500).json({ msg: err.message });
+  next(err);
+  // console.log(err);
+  // return res.status(500).json({ msg: err.message });
 };
 
 export default errorHandlerMiddleware;
--- src/api/helpers/middlewares/notFound.ts
+++ src/api/helpers/middlewares/notFound.ts
@@ -3,7 +3,7 @@ import { Request, Response, NextFunction } from "express";
 
 export const __404_err_page = (req: Request, res: Response, next: NextFunction) => {
   res.status(StatusCodes.NOT_FOUND).send("Error Page: Resource cannot be found!");
-  next();
+  // next();
 };
 
 // export default __404_err_page;
