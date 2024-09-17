d9c6b822c9661bb61681ce1433ff8604a2a68f8c
beauspot <beauspot@outlook.com>
2024-09-17 14:26:39 +0100

    slightly modified the clodinary config file

--- src/api/helpers/config/cloudinaryconfig.ts
+++ src/api/helpers/config/cloudinaryconfig.ts
@@ -5,9 +5,9 @@ import dotenv from "dotenv";
 dotenv.config();
 
 cloudinary.config({
-  cloud_name: process.env.CLOUD_NAME,
-  api_key: process.env.CLOUD_API_KEY,
-  api_secret: process.env.CLOUD_API_SECRET_KEY,
+      cloud_name: process.env.CLOUD_NAME,
+      api_key: process.env.CLOUD_API_KEY, 
+      api_secret: process.env.CLOUD_API_SECRET_KEY 
 });
 
 
@@ -29,3 +29,4 @@ export const cloudinaryUpload = async (
     });
   });
 };
+
