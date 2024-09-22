0cec9b9671a125265742ade16a1b6968380c83f1
beauspot <beauspot@outlook.com>
2024-09-22 20:56:17 +0100

    formated this service.

--- src/api/services/ProdCat.service.ts
+++ src/api/services/ProdCat.service.ts
@@ -5,75 +5,72 @@ import ProductCategoryModel from "@/models/Prod_categoryModels";
 import { ServiceAPIError } from "@/helpers/utils/custom-errors";
 import { ProductCategoryInterface } from "@/interfaces/prod_category_interface";
 
-
 @Service()
 export class ProductCategoryService {
-  constructor(@Inject(() => ProductCategoryModel) private productCatModel: typeof ProductCategoryModel){};
+  constructor(
+    @Inject(() => ProductCategoryModel)
+    private productCatModel: typeof ProductCategoryModel
+  ) {}
 
-  createCategoryService = async (
-   category: ProductCategoryInterface
- ) => {
-   const newCategory = await this.productCatModel.create({ ...category });
-   if (!newCategory) {
-     throw new ServiceAPIError (
-       "Could not create Category"
-     );
-   }
-   return newCategory;
- };
-
-
- updateCategoryService = async (
-  categoryID: string,
-  updateCategoryData: Partial<ProductCategoryInterface>
- ) => {
-  const updateCategory = await this.productCatModel.findByIdAndUpdate(
-    { _id: categoryID },
-    updateCategoryData,
-    {
-      new: true,
-      runValidators: true,
+  createCategoryService = async (category: ProductCategoryInterface) => {
+    const newCategory = await this.productCatModel.create({ ...category });
+    if (!newCategory) {
+      throw new ServiceAPIError("Could not create Category");
     }
-  );
-  validateMongoDbID(categoryID);
-  if (!updateCategory)
-    throw new ServiceAPIError (
-      `The Category ${categoryID} was not found to be updated`
-    );
-  return updateCategory;
- };
-  
- deleteCategoryService = async (categoryID: string) => {
-  const category = await this.productCatModel.findOneAndDelete({
-    _id: categoryID,
-  });
-  validateMongoDbID(categoryID);
-  if (!category)
-    throw new ServiceAPIError (
-      `The Category with the id: ${categoryID} was not found to be deleted`
-    );
-  return category;
- };
-
- getCategoryService = async (category: string) => {
-  const categoryExists = await this.productCatModel.findById({ _id: category });
-  validateMongoDbID(category);
-  if (!categoryExists)
-    throw new ServiceAPIError (
-      `The Product with the id: ${category} does not exist`
-    );
-  return categoryExists;
- };
-
- getAllCategoryService = async (): Promise<
-  ProductCategoryInterface[]
- > => {
-  const getAllCategories = await this.productCatModel.find();
-
-  if (getAllCategories.length <= 0) {
-    throw new ServiceAPIError (`No Product category found`);
+    return newCategory;
   };
-  
-  return getAllCategories;
- };
-};
+
+  updateCategoryService = async (
+    categoryID: string,
+    updateCategoryData: Partial<ProductCategoryInterface>
+  ) => {
+    const updateCategory = await this.productCatModel.findByIdAndUpdate(
+      { _id: categoryID },
+      updateCategoryData,
+      {
+        new: true,
+        runValidators: true,
+      }
+    );
+    validateMongoDbID(categoryID);
+    if (!updateCategory)
+      throw new ServiceAPIError(
+        `The Category ${categoryID} was not found to be updated`
+      );
+    return updateCategory;
+  };
+
+  deleteCategoryService = async (categoryID: string) => {
+    const category = await this.productCatModel.findOneAndDelete({
+      _id: categoryID,
+    });
+    validateMongoDbID(categoryID);
+    if (!category)
+      throw new ServiceAPIError(
+        `The Category with the id: ${categoryID} was not found to be deleted`
+      );
+    return category;
+  };
+
+  getCategoryService = async (category: string) => {
+    const categoryExists = await this.productCatModel.findById({
+      _id: category,
+    });
+    validateMongoDbID(category);
+    if (!categoryExists)
+      throw new ServiceAPIError(
+        `The Product with the id: ${category} does not exist`
+      );
+    return categoryExists;
+  };
+
+  getAllCategoryService = async (): Promise<ProductCategoryInterface[]> => {
+    const getAllCategories = await this.productCatModel.find();
+
+    if (getAllCategories.length <= 0) {
+      throw new ServiceAPIError(`No Product category found`);
+    }
+
+    return getAllCategories;
+  };
+}
