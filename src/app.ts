5b62cfcefd57df0606a83d1f710e69b6ed9e1998
beauspot <beauspot@outlook.com>
2024-10-06 17:15:21 +0100

    seperated the app and the server functionalities

--- src/app.ts
+++ src/app.ts
@@ -1,21 +1,22 @@
 // external dependencies
 import "reflect-metadata";
-import "express-async-errors";
-import express, { Application, NextFunction, Request, Response, Express } from "express";
-import morgan from "morgan";
 import cors from "cors";
+import path from "path";
+import YAML from "yamljs";
+import morgan from "morgan";
 import helmet from "helmet";
-import mongoSanitize from "express-mongo-sanitize";
-import { StatusCodes } from "http-status-codes";
-import bodyParser from "body-parser";
-import MongodbSession from "connect-mongodb-session";
-import cookieParser from "cookie-parser";
-import session from "express-session";
 import dotenv from "dotenv";
+import { Server } from "http";
+import "express-async-errors";
+import bodyParser from "body-parser";
+import session from "express-session";
+import cookieParser from "cookie-parser";
 import rateLimit from "express-rate-limit";
 import swaggerUI from "swagger-ui-express";
-import YAML from "yamljs";
-import path from "path";
+import { StatusCodes } from "http-status-codes";
+import mongoSanitize from "express-mongo-sanitize";
+import MongodbSession from "connect-mongodb-session";
+import express, { NextFunction, Request, Response, Express } from "express";
 
 // Reference path for sessions.
 /// <reference path="./api/types/express/custom.d.ts" />
@@ -31,7 +32,7 @@ const limiter = rateLimit({
 import { __404_err_page } from "@/middlewares/notFound";
 import errorHandlerMiddleware from "@/middlewares/errHandler";
 import connectDb from "@/config/dbconfig";
-import { runRedisOperation } from "@/config/redis.config";
+import runRedisOperation, { disconnectRedis } from "@/config/redis.config";
 import log from "@/utils/logger";
 
 // Routes Imports
@@ -48,23 +49,28 @@ import paymentRoute from "@/routes/payment.routes";
 import orderRoute from "@/routes/order.routes";
 
 dotenv.config();
+// const MongoDBStore = MongodbSession(session);
 
 export class EcomApp {
   private readonly app: Express;
   private readonly Port: number | string;
+  private server!: Server;
 
-  private MongoDBStore = MongodbSession(session);
-  private store = new this.MongoDBStore({
-    uri: process.env.MONGO_URL!,
-    collection: "Sessions-Collection",
-    expires: 60 * 60, // session will expire in 1hr
-  });
+  // private store = new MongoDBStore({
+  //   uri: process.env.MONGO_URL!,
+  //   collection: "Sessions-Collection",
+  //   expires: 60 * 60, // session will expire in 1hr
+  // });
 
   constructor() {
     this.app = express();
     this.Port = process.env.PORT || 4000;
     this.middleware();
     this.routes();
+  }
+
+  public getApp(): Express {
+    return this.app;
   }
 
   // Middleware functions
@@ -88,7 +94,7 @@ export class EcomApp {
         resave: false,
         secret: process.env.SESSION_SECRET_KEY!,
         saveUninitialized: false,
-        store: this.store,
+        // store: this.store,
         cookie: {
           sameSite: "strict",
           secure: false, // use true if using https
@@ -98,7 +104,7 @@ export class EcomApp {
     );
   }
 
-  private routes(): void {
+  public routes(): void {
     this.app.use("/api/v1/mall/user", authRoute);
     this.app.use("/api/v1/mall/products", productRoute);
     this.app.use("/api/v1/mall/blogs", blogRoute);
@@ -111,29 +117,40 @@ export class EcomApp {
     this.app.use("/api/v1/mall/payment", paymentRoute);
     this.app.use("/api/v1/mall/orders", orderRoute);
 
-    this.app.use("/", (_:Request, res: Response) => {
+    this.app.use(
+      (err: Error, req: Request, res: Response, next: NextFunction) => {
+        res.status(500).send(err.message);
+        // next();
+      }
+    );
+    this.app.use("/", (req: Request, res: Response, next: NextFunction) => {
       res
         .status(StatusCodes.PERMANENT_REDIRECT)
         .json({ message: "Welcome to the E-Commerce rest api application." });
     });
 
-    this.app.use((err: Error, req:Request, res:Response, next:NextFunction) => {
-      res.status(500).send(err.message);
-    })
-    
+    this.app.all("*", __404_err_page);
+
     this.app.use(errorHandlerMiddleware);
-    this.app.use("*", __404_err_page);
   }
 
-  async listen(): Promise<void> {
-    try {
-      await runRedisOperation();
-      await connectDb(process.env.MONGO_URL!);
-      this.app.listen(this.Port, () =>
-        log.info(`Server Listening on Port ${this.Port}`)
-      );
-    } catch (error: any) {
-      log.error(`There was an error with the server: ${error.message}`);
-    }
+  public listen(port: number | string, cb: () => void): void {
+    this.server = this.app.listen(port, cb);
+  }
+
+  public close(): Promise<void> {
+    return new Promise((resolve, reject) => {
+      if (this.server) {
+        this.server.close((err) => {
+          if (err) {
+            reject(err);
+          } else {
+            resolve();
+          }
+        });
+      } else {
+        resolve();
+      }
+    });
   }
 }
--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,41 @@
-import { EcomApp } from "@/app";
-import logger from "@/api/helpers/utils/logger";
+import dotenv from "dotenv";
 
-(() => {
+import { EcomApp } from "@/app";
+import logger from "@/utils/logger";
+import connectDb from "@/config/dbconfig";
+import runRedisOperation, { disconnectRedis } from "@/config/redis.config";
+
+dotenv.config();
+
+const app = new EcomApp();
+
+const startServer = async () => {
+  const Port: number | string = process.env.PORT || 4000;
   try {
-    const app = new EcomApp();
-    app.listen();
-  } catch (err: Error | any) {
-    logger.error(err.message);
+    app.listen(Port, () =>
+      logger.info(`Server Listening on http://localhost:${Port}`)
+    );
+    await runRedisOperation();
+    await connectDb(process.env.MONGO_URL!);
+  } catch (error: Error | any) {
+    logger.error(`There was an error with the server: ${error.message}`);
+    logger.error(`Error during server close: ${error.message}`);
+  }
+};
+
+const shutdown = async () => {
+  try {
+    await app.close();
+    await disconnectRedis();
+    logger.info("Server closed and Redis disconnected.");
+    process.exit(0);
+  } catch (error: Error | any) {
+    logger.error(`Error during server shutdown: ${error.message}`);
     process.exit(1);
   }
-})();
+};
+
+process.on("SIGINT", shutdown);
+process.on("SIGTERM", shutdown);
+
+startServer();
