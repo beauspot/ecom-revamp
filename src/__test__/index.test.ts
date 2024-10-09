da68f044039a0c53f06778034720c967bd84c149
beauspot <beauspot@outlook.com>
2024-10-09 10:59:25 +0100

    learning basics of jest

--- src/__test__/index.test.ts
+++ src/__test__/index.test.ts
@@ -1,16 +1,16 @@
-import { sumTotal, myFunction, fetchData } from "./sum";
+import { sumTotal, myFunction, fetchData, fetchPromise } from "./sum";
 
 test("adds 1 + 2 to equal 3", () => {
   expect(1 + 2).toBe(3);
 });
 
-describe("testing the sumTotal function", () => {
+describe("#testing the sumTotal function", () => {
   test("it adds 2 numbers together", () => {
     expect(sumTotal(2, 3)).toBe(5);
   });
 });
 
-describe("working with toEqual Matchers", () => {
+describe("#working with toEqual Matchers", () => {
   test("object assignment", () => {
     const data: Record<string, number> = { one: 1 };
     data["two"] = 2;
@@ -18,28 +18,28 @@ describe("working with toEqual Matchers", () => {
   });
 });
 
-describe("testing falsy matchers", () => {
+describe("#testing falsy matchers", () => {
   test("null is falsy", () => {
     const n = null;
     expect(n).toBeFalsy();
   });
 });
 
-describe("testing truthy matchers", () => {
+describe("#testing truthy matchers", () => {
   test("0 is falsy", () => {
     const n = 0;
     expect(n).toBeFalsy();
   });
 });
 
-describe("testing truthy matchers", () => {
+describe("#testing truthy matchers", () => {
   test("1 is truthy", () => {
     const n = 1;
     expect(n).toBeTruthy();
   });
 });
 
-describe("testing error handling with toThrow()", () => {
+describe("#testing error handling with toThrow()", () => {
   test("throws on an invalid input", () => {
     expect(() => {
       myFunction(null as unknown as string);
@@ -53,8 +53,10 @@ describe("testing error handling with toThrow()", () => {
   });
 });
 
+// Testing Async code with jest
+
 // using call backs for tests
-describe("writing async tests with callback functions", () => {
+describe("#writing async tests with callback functions", () => {
   test("the data is peanut butter", (done) => {
     function callback(data: any) {
       try {
@@ -65,5 +67,58 @@ describe("writing async tests with callback functions", () => {
       }
     }
     fetchData(callback);
+  });
+});
+
+describe("#testting fetchPromise method", () => {
+  test("resolve data should be peanut butter", () => {
+    return expect(fetchPromise()).resolves.toBe("peanut butter");
+  });
+
+  // test("The test fails with an error", () => {
+  //   return expect(fetchPromise()).rejects.toThrow('error')
+  // })
+});
+
+// testing with async/await
+describe("#Testing an asynchronous code with async/await", () => {
+  test("the data is peanut butter", async () => {
+    const data = await fetchPromise();
+    expect(data).toBe("peanut butter");
+  });
+});
+
+// mocks are fake implementations of real functions
+// spys are tools that are used to track the impmentations of those functions
+/**
+ * Jest.fn
+ * this is a way of creating a mock function & can be implemented
+ * to return a specific value or perform a specific action
+ */
+
+describe("#mocking implementaion of a basic function", () => {
+  test("mocking a function", () => {
+    const mock = jest.fn((x) => 42 + x);
+    expect(mock(1)).toBe(43);
+    expect(mock).toHaveBeenCalledWith(1);
+  });
+});
+
+/**
+ * Jest.spy
+ */
+
+describe("Spying on a method of an object", () => {
+  test("using the syp method in jest", () => {
+    const video = {
+      play() {
+        return true;
+      },
+    };
+    const spy = jest.spyOn(video, "play");
+    video.play();
+
+    expect(spy).toHaveBeenCalled();
+    spy.mockRestore();
   });
 });
