bdd81181700416179244673d2252e370849185cd
beauspot <beauspot@outlook.com>
2024-10-10 12:59:31 +0100

    to be ignored

--- src/__test__/sum.ts
+++ src/__test__/sum.ts
@@ -6,12 +6,53 @@ export function myFunction(input: string) {
   if (typeof input !== "string") {
     throw new Error("The input must be a string!");
   }
+}
+
+export function fetchData(callback: any) {
+  setTimeout(() => {
+    callback("peanut butter");
+  }, 1000);
 };
 
-export function fetchData(callback: any){
-    setTimeout(() => {
-        callback("peanut butter")
-    }, 1000);
+export interface IAuthData {
+  usernameToLower: string;
+  usernameCharacters: Array<string>;
+  userDetails: Object | undefined;
+  isAuthenticated: boolean;
+}
+
+export function authenticateuser(username: string, password: string): IAuthData {
+  const authStatus = username === "deveLOPER" && password === "dev";
+  return {
+    usernameToLower: username.toLowerCase(),
+    usernameCharacters: username.split(""),
+    userDetails: {},
+    isAuthenticated: authStatus,
+  };
+}
+
+export function fetchPromise() {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => resolve("peanut butter"), 1000);
+  });
+}
+
+// from j-grace coder
+export function product(a: number, b: number): number {
+  return a * b;
+};
+
+export function usernameLowerCase(username: string) {
+  return username.toLowerCase()
+}
+
+export class UserNameToLowerCase {
+  public toLower(username: string) {
+    if (username === "") {
+      throw new Error("A valid username must be provided");
+    }
+    return username.toLowerCase();
+  }
 }
 
 // matches in jest
