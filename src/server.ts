bac1863c41d72fd4eca7827cb355af80cd3347ce
beauspot <beauspot@outlook.com>
2024-10-12 21:03:07 +0100

    revamped server.ts file

--- src/server.ts
+++ src/server.ts
@@ -1,41 +1,125 @@
-import dotenv from "dotenv";
+import "reflect-metadata";
+import cors from "cors";
+import path from "path";
+import YAML from "yamljs";
+import morgan from "morgan";
+import helmet from "helmet";
+import { Server } from "http";
+import "express-async-errors";
+import session from "express-session";
+import cookieParser from "cookie-parser";
+import rateLimit from "express-rate-limit";
+import swaggerUI from "swagger-ui-express";
+import { StatusCodes } from "http-status-codes";
+import mongoSanitize from "express-mongo-sanitize";
+import MongodbSession from "connect-mongodb-session";
+import express, { NextFunction, Request, Response, Express } from "express";
 
-import { EcomApp } from "@/app";
-import logger from "@/utils/logger";
-import connectDb from "@/config/dbconfig";
-import runRedisOperation, { disconnectRedis } from "@/config/redis.config";
+import { __404_err_page } from "@/middlewares/notFound";
+import errorHandlerMiddleware from "@/middlewares/errHandler";
 
-dotenv.config();
+// Routes Imports
+import authRoute from "@/routes/auth.routes";
+import colorRoute from "@/routes/color.routes";
+import productRoute from "@/routes/product.routes";
+import enquiryRoute from "@/routes/enq.routes";
+import blogRoute from "@/routes/blog.routes";
+import blogCategoryRoute from "@/routes/blogCategory.routes";
+import productCategoryRoute from "@/routes/productCategory.routes";
+import brandRoute from "@/routes/brands.routes";
+import couponRoute from "@/routes/coupon.routes";
+import paymentRoute from "@/routes/payment.routes";
+import orderRoute from "@/routes/order.routes";
 
-const app = new EcomApp();
+// Reference path for sessions.
+/// <reference path="./api/types/express/custom.d.ts" />
 
-const startServer = async () => {
-  const Port: number | string = process.env.PORT || 4000;
-  try {
-    app.listen(Port, () =>
-      logger.info(`Server Listening on http://localhost:${Port}`)
-    );
-    await runRedisOperation();
-    await connectDb(process.env.MONGO_URL!);
-  } catch (error: Error | any) {
-    logger.error(`There was an error with the server: ${error.message}`);
-    logger.error(`Error during server close: ${error.message}`);
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  max: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
+  standardHeaders: true,
+  legacyHeaders: false,
+});
+
+function createServer() {
+  
+  // let server!: Server;
+
+  const app = express();
+
+  app.use(express.json());
+  app.use(morgan("combined"));
+  app.use(limiter);
+  app.use(cors());
+  app.use(helmet());
+  app.use(mongoSanitize());
+  app.use(cookieParser());
+  app.use(express.urlencoded({ extended: false }));
+  if (process.env.NODE_ENV !== "production") {
+    app.use(morgan("tiny"));
   }
-};
+  app.use(
+    session({
+      resave: false,
+      secret: process.env.SESSION_SECRET_KEY!,
+      saveUninitialized: false,
+      // store: this.store,
+      cookie: {
+        sameSite: "strict",
+        secure: false, // use true if using https
+        maxAge: 1000 * 60 * 60, // cookie would expire in 1 hour
+      },
+    })
+  );
 
-const shutdown = async () => {
-  try {
-    await app.close();
-    await disconnectRedis();
-    logger.info("Server closed and Redis disconnected.");
-    process.exit(0);
-  } catch (error: Error | any) {
-    logger.error(`Error during server shutdown: ${error.message}`);
-    process.exit(1);
-  }
-};
+  app.use("/api/v1/mall/user", authRoute);
+  app.use("/api/v1/mall/products", productRoute);
+  app.use("/api/v1/mall/blogs", blogRoute);
+  app.use("/api/v1/mall/brand", brandRoute);
+  app.use("/api/v1/mall/blogscategory", blogCategoryRoute);
+  app.use("/api/v1/mall/productscategory", productCategoryRoute);
+  app.use("/api/v1/mall/coupon", couponRoute);
+  app.use("/api/v1/mall/colors", colorRoute);
+  app.use("/api/v1/mall/enquiry", enquiryRoute);
+  app.use("/api/v1/mall/payment", paymentRoute);
+  app.use("/api/v1/mall/orders", orderRoute);
 
-process.on("SIGINT", shutdown);
-process.on("SIGTERM", shutdown);
+  app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
+    res.status(500).send(err.message);
+    // next();
+  });
+  app.use("/", (req: Request, res: Response, next: NextFunction) => {
+    res
+      .status(StatusCodes.PERMANENT_REDIRECT)
+      .json({ message: "Welcome to the E-Commerce rest api application." });
+  });
 
-startServer();
+  app.all("*", __404_err_page);
+
+  app.use(errorHandlerMiddleware);
+
+  return app;
+}
+
+export default createServer;
+
+/**
+ * New format for server.ts file: import express from "express";
+ * 
+import routes from "../routes";
+import deserializeUser from "../middleware/deserializeUser";
+
+function createServer() {
+  const app = express();
+
+  app.use(express.json());
+
+  app.use(deserializeUser);
+
+  routes(app);
+
+  return app;
+}
+
+export default createServer;
+ */
